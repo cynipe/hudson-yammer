b63085c997d44f55dad34f75ae72ba129b124f18
russellxhart <russellxhart@402f72b4-6722-11de-ba0c-230037762ff9>
2009-07-06 16:16:35 +0000

    Find group from group name changes
    git-svn-id: http://hudson-yammer.googlecode.com/svn/trunk@5 402f72b4-6722-11de-ba0c-230037762ff9

--- yammerBuildNotifier/src/main/java/rsh/hudson/plugin/YammerUtils.java
+++ yammerBuildNotifier/src/main/java/rsh/hudson/plugin/YammerUtils.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -18,12 +17,43 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HTTP;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
 
 public class YammerUtils {
+	/**
+	 * Restful Yammer URL for groups api.
+	 */
+	private static final String YAMMER_API_V1_GROUPS = "https://www.yammer.com/api/v1/groups";
+
+	private static final String MESSAGE_GROUP_ID_PARAM_NAME = "group_id";
+
+	private static final String MESSAGE_BODY_PARAM_NAME = "body";
+
+	/**
+	 * Restful Yammer URL for messages api.
+	 */
+	private static final String YAMMER_API_V1_MESSAGES = "https://www.yammer.com/api/v1/messages";
+
+	private static final String AUTHORIZATION_HEADER = "Authorization";
+
+	/**
+	 * Yammer URL for getting access token.
+	 */
+	private static final String OAUTH_ACCESS_TOKEN_URL = "https://www.yammer.com/oauth/access_token?callback_token=";
+
+	/**
+	 * Yammer URL for getting request token.
+	 */
+	private static final String OAUTH_REQUEST_TOKEN_URL = "https://www.yammer.com/oauth/request_token";
+
 	/**
 	 * The Yammer generated Key for this app.
 	 */
@@ -34,10 +64,23 @@ public class YammerUtils {
 	 */
 	private static final String SECRET = "4SjnsIFO9cyUCpU3u3TfZqSnVMhZ6vG1Dwwt0SvFg";
 
+	/**
+	 * Constant for oauth_token query string parameter name.
+	 */
 	public static final String OAUTH_TOKEN = "oauth_token";
 
+	/**
+	 * Constant for oauth_token_secret query string parameter name.
+	 */
 	public static final String OAUTH_SECRET = "oauth_token_secret";
 
+	/**
+	 * Creates a string of the oauth_headers for this plugin.
+	 * 
+	 * @param token
+	 * @param token_secret
+	 * @return
+	 */
 	private static String oauth_headers(String token, String token_secret) {
 		StringBuffer buff = new StringBuffer();
 		buff.append("OAuth realm=\"");
@@ -69,16 +112,27 @@ public class YammerUtils {
 		return buff.toString();
 	}
 
+	/**
+	 * Gets a new set of request token parameters for this plugin. These
+	 * parameters are used in getting an access token for the plugin. Please not
+	 * that request tokens expire.
+	 * 
+	 * @return
+	 * @throws ClientProtocolException
+	 * @throws IOException
+	 */
 	public static Map<String, String> getRequestTokenParameters()
 			throws ClientProtocolException, IOException {
+
 		HttpClient httpclient = new DefaultHttpClient();
 		try {
-			HttpPost httpost = new HttpPost(
-					"https://www.yammer.com/oauth/request_token");
-			httpost.addHeader("Authorization", oauth_headers(null, null));
+			HttpPost httpost = new HttpPost(OAUTH_REQUEST_TOKEN_URL);
+			httpost.addHeader(AUTHORIZATION_HEADER, oauth_headers(null, null));
 
 			BufferedReader reader = getResponseReader(httpclient
 					.execute(httpost));
+
+			// Request token parameters are on the first line of the response
 			String requestTokenResponseString = reader.readLine();
 			return parseQueryStringParameters(requestTokenResponseString);
 		} finally {
@@ -86,19 +140,32 @@ public class YammerUtils {
 		}
 	}
 
+	/**
+	 * Gets the access token parameters for this plugin using the supplied
+	 * request token parameters and user authenticated access token.
+	 * 
+	 * @param requestAuthToken
+	 * @param requestAuthSecret
+	 * @param accessToken
+	 * @return
+	 * @throws ClientProtocolException
+	 * @throws IOException
+	 */
 	public static Map<String, String> getAccessTokenParameters(
 			String requestAuthToken, String requestAuthSecret,
 			String accessToken) throws ClientProtocolException, IOException {
+
 		HttpClient httpclient = new DefaultHttpClient();
 		try {
-			HttpPost httpost = new HttpPost(
-					"https://www.yammer.com/oauth/access_token?callback_token="
-							+ accessToken);
-			httpost.addHeader("Authorization", oauth_headers(requestAuthToken,
-					requestAuthSecret));
+			HttpPost httpost = new HttpPost(OAUTH_ACCESS_TOKEN_URL
+					+ accessToken);
+			httpost.addHeader(AUTHORIZATION_HEADER, oauth_headers(
+					requestAuthToken, requestAuthSecret));
 
 			BufferedReader reader = getResponseReader(httpclient
 					.execute(httpost));
+
+			// Access token parameters are on the first line of the response
 			String accessTokenResponseString = reader.readLine();
 			return parseQueryStringParameters(accessTokenResponseString);
 		} finally {
@@ -106,36 +173,67 @@ public class YammerUtils {
 		}
 	}
 
+	/**
+	 * Post a message to Yammer within the supplied group.
+	 * 
+	 * @param accessAuthToken
+	 * @param accessAuthSecret
+	 * @param message
+	 * @param group
+	 * @throws ClientProtocolException
+	 * @throws IOException
+	 */
 	public static void sendMessage(String accessAuthToken,
-			String accessAuthSecret, String message, String group) {
+			String accessAuthSecret, String message, String group)
+			throws ClientProtocolException, IOException {
 		HttpClient httpclient = new DefaultHttpClient();
 
 		try {
-			HttpPost httpPost = new HttpPost(
-					"https://www.yammer.com/api/v1/messages");
-			httpPost.addHeader("Authorization", oauth_headers(accessAuthToken,
-					accessAuthSecret));
+			HttpPost httpPost = new HttpPost(YAMMER_API_V1_MESSAGES);
+			httpPost.addHeader(AUTHORIZATION_HEADER, oauth_headers(
+					accessAuthToken, accessAuthSecret));
 
 			List<NameValuePair> nvps = new ArrayList<NameValuePair>();
-			nvps.add(new BasicNameValuePair("body", message));
+			nvps.add(new BasicNameValuePair(MESSAGE_BODY_PARAM_NAME, message));
 
-			if (!group.equals("")) {
-				nvps.add(new BasicNameValuePair("group_id", group));
+			// If a group name is supplied find the group id and post the
+			// message to it
+			if (group != null && !group.equals("")) {
+				nvps.add(new BasicNameValuePair(MESSAGE_GROUP_ID_PARAM_NAME,
+						group));
 			}
 
 			httpPost.setEntity(new UrlEncodedFormEntity(nvps, HTTP.UTF_8));
 			httpclient.execute(httpPost);
-		} catch (UnsupportedEncodingException e) {
-			throw new RuntimeException(e);
-		} catch (ClientProtocolException e) {
-			throw new RuntimeException(e);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
 		} finally {
 			httpclient.getConnectionManager().shutdown();
 		}
 	}
 
+	/**
+	 * Post a message to Yammer.
+	 * 
+	 * @param accessAuthToken
+	 * @param accessAuthSecret
+	 * @param message
+	 * @throws ClientProtocolException
+	 * @throws IOException
+	 */
+	public static void sendMessage(String accessAuthToken,
+			String accessAuthSecret, String message)
+			throws ClientProtocolException, IOException {
+
+		sendMessage(accessAuthToken, accessAuthSecret, message, "");
+	}
+
+	/**
+	 * Get a reader for the supplied HttpResponse.
+	 * 
+	 * @param response
+	 * @return
+	 * @throws IllegalStateException
+	 * @throws IOException
+	 */
 	private static BufferedReader getResponseReader(HttpResponse response)
 			throws IllegalStateException, IOException {
 		HttpEntity entity = response.getEntity();
@@ -147,8 +245,15 @@ public class YammerUtils {
 		return null;
 	}
 
+	/**
+	 * Parse a string of query string parameters into a Map.
+	 * 
+	 * @param queryString
+	 * @return
+	 */
 	private static Map<String, String> parseQueryStringParameters(
 			String queryString) {
+
 		Map<String, String> parametersMap = new HashMap<String, String>();
 		StringTokenizer tokenizerNameValuePair = new StringTokenizer(
 				queryString, "&");
@@ -171,4 +276,60 @@ public class YammerUtils {
 		return parametersMap;
 	}
 
+	/**
+	 * Gets the group id of the supplied group name.
+	 * 
+	 * @param accessAuthToken
+	 * @param accessAuthSecret
+	 * @param groupName
+	 * @return
+	 * @throws IllegalStateException
+	 * @throws ClientProtocolException
+	 * @throws IOException
+	 * @throws DocumentException
+	 * @throws JDOMException
+	 */
+	@SuppressWarnings("unchecked")
+	public static String getGroupId(String accessAuthToken,
+			String accessAuthSecret, String groupName)
+			throws IllegalStateException, ClientProtocolException, IOException,
+			DocumentException {
+
+		HttpClient httpclient = new DefaultHttpClient();
+
+		try {
+			Integer currentPage = 1;
+			Integer responses = null;
+			String groupId = null;
+
+			// Yammer returns 20 results per page so need to go through all the
+			// pages trying to find the group
+			while ((responses == null || responses != 0) && groupId == null) {
+				HttpGet httpGet = new HttpGet(YAMMER_API_V1_GROUPS + "?page="
+						+ currentPage + "&letter=" + groupName.substring(0, 1));
+				httpGet.addHeader(AUTHORIZATION_HEADER, oauth_headers(
+						accessAuthToken, accessAuthSecret));
+
+				SAXReader reader = new SAXReader();
+				Document doc = reader.read(getResponseReader(httpclient
+						.execute(httpGet)));
+
+				List nodes = doc.selectNodes("/response/response");
+				responses = nodes.size();
+
+				if (responses > 0) {
+					Node node = doc
+							.selectSingleNode("/response/response[full-name='"
+									+ groupName + "']/id");
+					groupId = node != null ? node.getStringValue() : null;
+				}
+
+				currentPage++;
+			}
+
+			return groupId;
+		} finally {
+			httpclient.getConnectionManager().shutdown();
+		}
+	}
 }
